3062fd82f1ef1d6fb74caf9bff5d9fc60d61a8ad
SparshS01 <srivastavasparsh088@gmail.com>
2025-07-31 03:25:42 +0530

    Create Program1.cpp

---
+++ Program1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1,num2;
+    cout<<"enter number 1 ";
+    cin>>num1;
+    cout<<"enter number 2 ";
+    cin>>num2;
+    int bitwise_and=num1&num2;
+    int bitwise_or=num1|num2;
+    int bitwise_xor=num1^num2;
+    int bitwise_not=~num1;
+    int bitwise_leftshift=num1<<1;
+    int bitwise_rightshift=num2>>1;
+    cout<<"binary AND operation: "<<bitwise_and<<endl;
+    cout<<"binary OR operation: "<<bitwise_or<<endl;
+    cout<<"binary XOR operation: "<<bitwise_xor<<endl;
+    cout<<"binary NOT operation: "<<bitwise_not<<endl;
+    cout<<"binary LEFTSHIFT operation: "<<bitwise_leftshift<<endl;
+    cout<<"binary RIGHTSHIFT operation: "<<bitwise_rightshift<<endl;
+}
